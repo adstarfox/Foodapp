4885f2f558a0df78a8aeae6fb8c56431ac2ee9fb
Alex Durrant <alexjdurrant@gmail.com>
2023-07-20 10:07:46 -0600

    saving

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,7 +2,7 @@ import { useReducer } from "react"
 
 import CartContext from "./cart-context"
 
-const defaoultCartState = {
+const defaultCartState = {
     items: [],
     totalAmount: 0
 }
@@ -11,12 +11,16 @@ const cartReducer = (state, action) => {
     if (action.type === 'ADD'){
         const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
     }
-    return defaoultCartState
+    return defaultCartState
 } 
 
 const CartProvider = props => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaoultCartState)
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
     const addItemToCartHander = item => {
         dispatchCartAction({type: 'ADD', item: item})
